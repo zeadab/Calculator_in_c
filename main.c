76a701aac85a280387212cf9827c59607c7aa129
zeadab <ist.abadi.ali@gmail.com>
2026-03-10 19:11:46 +0100

    programm is finished , bbut it has a bug

--- main.c
+++ main.c
@@ -2,6 +2,30 @@
 
 int main(void)
 {
-    printf("Hello, World!\n");
+    double num1 ;
+    double num2 ;
+    char operator ;
+    double result;
+
+    printf("Enter the first number please: ");
+    scanf("%lf", &num1);
+    printf("which Operator (+,*,-;/)? ");
+    scanf("%c", &operator);
+    printf("Enter the second number please: ");
+    scanf("%lf", &num2);
+
+    switch(operator)
+    {
+        case '+': result = num1 + num2;
+        break;
+        case '-': result = num1 - num2;
+        break;
+        case '*': result = num1 * num2;
+        break;
+        case '/': result = num1 / num2;
+        break;
+        default:printf("please Enter a Operator");
+    }
+    printf("The result is: %lf", result);
     return 0;
 }
